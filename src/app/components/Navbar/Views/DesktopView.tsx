17dfea2c943c6db715f650d7a9061e2c6f96d41b
Terrance <terrancem.brown@gmail.com>
2025-12-30 23:27:36 -0600

    Updated Navbar with Mobile and Desktop Views. Not fully working yet

---
+++ src/app/components/Navbar/Views/DesktopView.tsx
@@ -0,0 +1,9 @@
+const DesktopView = () => {
+    return (
+        <nav>
+            This is the Desktop Menu
+        </nav>
+    )
+};
+
+export { DesktopView };
---
+++ src/app/components/Navbar/Views/MobileView.tsx
@@ -0,0 +1,12 @@
+const MobileView = () => {
+    const styles = {
+        borders: "border-b border-black"
+    };
+    return (
+        <nav className={`${styles.borders}`}>
+            This is the Mobile Menu
+        </nav>
+    )
+};
+
+export { MobileView };
